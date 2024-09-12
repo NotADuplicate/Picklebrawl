0c8859bcf8bfdc7fd18369f82e9a7e8dd80f81d2
Rachel Emrick <remrick@email.unc.edu>
2024-09-11 23:22:38 -0400

    Better SQL tables to store match history

--- server/database.js
+++ server/database.js
@@ -72,26 +72,19 @@ db.serialize(() => {
     );`);
 
     // Make a table where each row contains a single historical match
-    db.run(`CREATE TABLE IF NOT EXISTS matchHistory (
+    db.run(`CREATE TABLE IF NOT EXISTS match_history (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        challenger_team_id INT NOT NULL,
-        challenged_team_id INT NOT NULL,
+        league_id INT NOT NULL,
         home_team_id INT NOT NULL,
         away_team_id INT NOT NULL,
         home_team_score INT NOT NULL,
         away_team_score INT NOT NULL,
         weather TEXT NOT NULL,
-        FOREIGN KEY (challenger_team_id) REFERENCES teams(id),
-        FOREIGN KEY (challenged_team_id) REFERENCES teams(id),
+        FOREIGN KEY (league_id) REFERENCES leagues(id),
         FOREIGN KEY (home_team_id) REFERENCES teams(id),
         FOREIGN KEY (away_team_id) REFERENCES teams(id)
     );`);
 
-    /* SQL is something I don't understand super well yet so this may not be a good implementation, 
-    but I think the matchHistory table should have a column where each row (match) can link to a 
-    table of matchTicksHistory (below). Each matchTicksHistory table should have 40 (or however 
-    many ticks we want per game) rows, and each row stores what happened in that tick. Thoughts?
-    */
     /* Store: who has possession,
     where the ball is (also tells how much advancement has happened since we'll have a log),
     roles of each player and their team (unsure how to implement cleanly -- I've added a 
@@ -101,63 +94,60 @@ db.serialize(() => {
     who attempted to score and if they succeeded (can only allow 1 attempt per tick if that's ok),
     maybe in the future implement log of random events or ways quirks interacted
     */
-    db.run(`CREATE TABLE IF NOT EXISTS matchTicksHistory (
-        tick INTEGER PRIMARY KEY AUTOINCREMENT,
+    db.run(`CREATE TABLE IF NOT EXISTS match_ticks_history (
+        tick INT NOT NULL,
+        match_id INT NOT NULL,
         possession_team_id INT NOT NULL,
         ball_position INT NOT NULL,
-        home_team_roles INT NOT NULL,
-        away_team_roles INT NOT NULL,
-        attacks INT NOT NULL,
-        score_was_attempted BOOLEAN NOT NULL,
-        successful_score BOOLEAN NOT NULL,
-        attempted_scorer_player_id INT,
+        FOREIGN KEY (match_id) REFERENCES match_history(id),
         FOREIGN KEY (possession_team_id) REFERENCES teams(id),
-        FOREIGN KEY (home_team_roles) REFERENCES playedTeamHistory(id),
-        FOREIGN KEY (away_team_roles) REFERENCES playedTeamHistory(id),
-        FOREIGN KEY (attacks) REFERENCES attackHistory(id),
-        FOREIGN KEY (attempted_scorer_player_id) REFERENCES players(id)
-    );`);
-
-    // Maybe this table could store the combinations of players used and their roles
-    // Could we store roles as an enum/ints?
-    // It should hold references to targets as well
-    // Maybe ticks 0-19 in a game refer to id 0, then ticks 20-39 refer to id 1 if a player was swapped
-    // at halftime or their role changed
-    // I left player 4 as optionally null because of the "huge" quirk
-    db.run(`CREATE TABLE IF NOT EXISTS playedTeamHistory (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        player1_hist_id INT NOT NULL,
-        player2_hist_id INT NOT NULL,
-        player3_hist_id INT NOT NULL,
-        player4_hist_id INT,
-        FOREIGN KEY (player1_hist_id) REFERENCES playerHistory(id),
-        FOREIGN KEY (player2_hist_id) REFERENCES playerHistory(id),
-        FOREIGN KEY (player3_hist_id) REFERENCES playerHistory(id),
-        FOREIGN KEY (player4_hist_id) REFERENCES playerHistory(id)
+        PRIMARY KEY (match_id, tick)
     );`);
 
     // Roles set as ints so we can maybe set them as an enum or something
-    db.run(`CREATE TABLE IF NOT EXISTS playerHistory (
+    db.run(`CREATE TABLE IF NOT EXISTS player_history (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        match_id INT NOT NULL,
+        tick_start INT NOT NULL,
+        tick_end INT NOT NULL,
         player_id INT NOT NULL,
         offensive_role INT NOT NULL,
         offensive_target_id INT,
         defensive_role INT NOT NULL,
         defensive_target_id INT,
+        FOREIGN KEY (match_id) REFERENCES match_history(id),
+        FOREIGN KEY (tick_start) REFERENCES match_history(tick),
+        FOREIGN KEY (tick_end) REFERENCES match_history(tick),
         FOREIGN KEY (player_id) REFERENCES players(id),
         FOREIGN KEY (offensive_target_id) REFERENCES players(id),
         FOREIGN KEY (defensive_target_id) REFERENCES players(id)
     );`);
 
-    // I'm actually not sure if damage done is an int, that may need to be corrected
-    db.run(`CREATE TABLE IF NOT EXISTS attackHistory (
+    db.run(`CREATE TABLE IF NOT EXISTS attack_history (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        match_id INT NOT NULL,
+        tick INT NOT NULL,
         attacking_player_id INT NOT NULL,
         attacked_player_id INT NOT NULL,
-        damage_done INT NOT NULL,
+        damage_done FLOAT NOT NULL,
         permanent_injury BOOLEAN NOT NULL,
+        FOREIGN KEY (match_id) REFERENCES match_history(id),
+        FOREIGN KEY (tick) REFERENCES match_history(tick),
         FOREIGN KEY (attacking_player_id) REFERENCES players(id),
         FOREIGN KEY (attacked_player_id) REFERENCES players(id)
+    );`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS scoring_history (
+        match_id INT NOT NULL,
+        tick INT NOT NULL,
+        shooter_id INT NOT NULL,
+        successful_score BOOLEAN NOT NULL,
+        team_id INT NOT NULL,
+        FOREIGN KEY (match_id) REFERENCES match_history(id),
+        FOREIGN KEY (tick) REFERENCES match_history(tick),
+        FOREIGN KEY (shooter_id) REFERENCES players(id),
+        FOREIGN KEY (team_id) REFERENCES teams(id)
+        PRIMARY KEY (match_id, tick)
     );`);
 
 });
